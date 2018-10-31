999df940345d494d2b864f657cf81869a913c1d6
fowlerbr <brennan.fowler@mail.utoronto.ca>
2018-10-31 11:54:09 -0400

    Added structure for 'bundle's in search results. Created a new type of search result, different than just a textbook, called a bundle. Added a "view textbooks" button that shows a modal with all textbook information inside the bundle.

--- src/js/search.js
+++ src/js/search.js
@@ -5,3 +5,29 @@ const sliderValue = document.querySelector("#slider-value");
 slider.oninput = function() {
     sliderValue.innerHTML = this.value / 10;
 }
+
+// Get the modal
+var modal = document.getElementById('modal-backdrop');
+
+// Get the button that opens the modal
+const btn = document.querySelector(".view-bundle");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
