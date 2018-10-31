71bc944cd9174e66ce8670470ee191bee737e304
fowlerbr <brennan.fowler@mail.utoronto.ca>
2018-10-31 13:18:33 -0400

    Added a seller info pop-up modal.

--- src/js/search.js
+++ src/js/search.js
@@ -1,33 +1,35 @@
+// Configure slider
 const slider = document.querySelector("#condition-slider");
 const sliderValue = document.querySelector("#slider-value");
 
-// Update slider
 slider.oninput = function() {
     sliderValue.innerHTML = this.value / 10;
 }
 
-// Get the modal
-var modal = document.getElementById('modal-backdrop');
+// Configure bundle pop-up modal
+const bundleModal = document.querySelector("#bundle-backdrop");
+const bundleOpenButton = document.querySelector(".view-bundle");
+const bundleCloseButton = document.querySelector("#bundle-close");
 
-// Get the button that opens the modal
-const btn = document.querySelector(".view-bundle");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
+bundleOpenButton.onclick = function() {
+    bundleModal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+bundleCloseButton.onclick = function() {
+    bundleModal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+// Configure seller-info pop-up modal
+const sellerModal = document.querySelector("#seller-backdrop");
+const sellerOpenButtons = document.querySelectorAll(".contact-seller");
+const sellerCloseButton = document.querySelector("#seller-close");
+
+for (let i = 0; i < sellerOpenButtons.length; i++) {
+    sellerOpenButtons[i].onclick = function() {
+        sellerModal.style.display = "block";
     }
+}
+
+sellerCloseButton.onclick = function () {
+    sellerModal.style.display = "none";
 }
