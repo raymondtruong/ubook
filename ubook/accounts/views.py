7df9084f7756aea6cc869a2fe8f77c8424acd2b6
Christopher Chiu <chr.chiu@mail.utoronto.ca>
2018-10-31 15:30:48 -0400

    added views for listing and authentication pages

--- ubook/accounts/views.py
+++ ubook/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.http import HttpResponse
 
 
 def register(request):
--- ubook/listings/views.py
+++ ubook/listings/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from listings.models import *
 
-
 def index(request):
-    return render(request, "listings/index.html")
+    if request.user.is_authenticated:
+        t = Textbook(name="Textbook", edition="1", condition="9.5",
+                     description="This is a textbook.", authors="Author 1",
+                     courses="CSC123")
+        t.save()
+
+        l = Listing(title="Listing", price="123.45", owner=request.user,
+                    photo="https://banner2.kisspng.com/20180105/prq/book-clip-art-5a5019c8436fc6.28019198151519892027624341.jpg")
+
+        l.save()
+        l.textbooks.add(t)
+        l.save()
+
+        listings = Listing.objects.all()
+        return render(request, "listings/index.html", {"listings": listings})
+    else:
+        return render(request, "listings/home.html")
+
 
 def create(request):
     return render(request, "listings/create.html")
