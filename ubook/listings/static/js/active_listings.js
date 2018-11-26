538761063a809863be2dfa262cc9b54b05aa8c15
Jacob Gigli <jacob.gigli@mail.utoronto.ca>
2018-11-26 13:28:06 -0500

    Fixed delete listing bug

--- ubook/listings/static/js/active_listings.js
+++ ubook/listings/static/js/active_listings.js
@@ -1,9 +1,20 @@
 "use strict"; 
-const listings = document.querySelector('#results')
+const bundleModal = document.querySelector("#bundle-backdrop");
+const bundleOpenButton = document.querySelector(".view-bundle");
+const bundleCloseButton = document.querySelector("#bundle-close");
+const listings = document.querySelector('#results');
 listings.addEventListener('click', remove_listings)
+
+bundleOpenButton.onclick = function() {
+    bundleModal.style.display = "block";
+}
+
+bundleCloseButton.onclick = function() {
+    bundleModal.style.display = "none";
+}
 function remove_listings(e){
 	e.preventDefault()
-	if (e.target.classList.contains('x')) {
+	if (e.target.classList.contains('remove-listing')) {
         console.log(e.target)	
 		const listingToRemove = e.target.parentElement.parentElement
 		listings.removeChild(listingToRemove)
