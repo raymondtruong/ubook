11a1ebc29871dc728749836ba183d9af995739fd
Jacob Gigli <jacob.gigli@mail.utoronto.ca>
2018-11-27 14:46:15 -0500

    Added edit listings template

--- ubook/listings/static/js/active_listings.js
+++ ubook/listings/static/js/active_listings.js
@@ -1,9 +1,13 @@
 "use strict"; 
 const bundleModal = document.querySelector("#bundle-backdrop");
+const editModal = document.querySelector("#edit-backdrop");
 const bundleOpenButton = document.querySelector(".view-bundle");
 const bundleCloseButton = document.querySelector("#bundle-close");
+const editCloseButton = document.querySelector("#edit-close");
 const listings = document.querySelector('#results');
 listings.addEventListener('click', remove_listings)
+listings.addEventListener('click', edit_listings)
+
 
 bundleOpenButton.onclick = function() {
     bundleModal.style.display = "block";
@@ -11,6 +15,10 @@ bundleOpenButton.onclick = function() {
 
 bundleCloseButton.onclick = function() {
     bundleModal.style.display = "none";
+}
+
+editCloseButton.onclick = function() {
+    editModal.style.display = "none";
 }
 function remove_listings(e){
 	e.preventDefault()
@@ -21,4 +29,11 @@ function remove_listings(e){
 
 	}
 
+}
+function edit_listings(e){
+	e.preventDefault()
+	if (e.target.classList.contains('edit-listing')) {
+	     editModal.style.display = "block";
+	}
+	
 }
