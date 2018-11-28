47ca3843bba936e2b3c513d451abdc9234ac5002
jacobgigli <gigli01.jacob@gmail.com>
2018-11-28 18:14:58 -0500

    Finished edit listing function for text Books with no bundles

--- ubook/listings/static/js/active_listings.js
+++ ubook/listings/static/js/active_listings.js
@@ -5,8 +5,11 @@ const bundleOpenButton = document.querySelector(".view-bundle");
 const bundleCloseButton = document.querySelector("#bundle-close");
 const editCloseButton = document.querySelector("#edit-close");
 const listings = document.querySelector('#results');
+const editlisting = document.querySelector('#edit-backdrop');
+let listingToBeEdited;
 listings.addEventListener('click', remove_listings)
 listings.addEventListener('click', edit_listings)
+editlisting.addEventListener('click', submit_edited_listings)
 
 
 bundleOpenButton.onclick = function() {
@@ -23,7 +26,6 @@ editCloseButton.onclick = function() {
 function remove_listings(e){
 	e.preventDefault()
 	if (e.target.classList.contains('remove-listing')) {
-        console.log(e.target)	
 		const listingToRemove = e.target.parentElement.parentElement
 		listings.removeChild(listingToRemove)
 
@@ -34,6 +36,22 @@ function edit_listings(e){
 	e.preventDefault()
 	if (e.target.classList.contains('edit-listing')) {
 	     editModal.style.display = "block";
+	     listingToBeEdited = e.target.parentElement.parentElement
+	     console.log(listingToBeEdited)
 	}
 	
+}
+function submit_edited_listings(e){
+	e.preventDefault()
+	if (e.target.classList.contains('submit-edit-listing')) {
+	     const editfields = e.target.parentElement.parentElement
+	     listingToBeEdited.getElementsByClassName("book-title")[0].textContent = editfields.querySelector("#edit-title-input").value
+	     listingToBeEdited.getElementsByClassName("book-authors")[0].textContent = editfields.querySelector("#edit-author-input").value
+	     listingToBeEdited.getElementsByClassName("book-courses")[0].textContent = editfields.querySelector("#edit-course-input").value
+	     listingToBeEdited.getElementsByClassName("book-condition")[0].textContent = editfields.querySelector("#edit-condition-input").value
+	     listingToBeEdited.getElementsByClassName("book-description")[0].textContent = editfields.querySelector("#edit-description-input").value
+	     listingToBeEdited.getElementsByClassName("book-price")[0].textContent = editfields.querySelector("#edit-price-input").value
+	     editModal.style.display = "none";
+	}
+
 }
